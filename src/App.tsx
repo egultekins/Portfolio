4bfac14012ba9f391f6fc92b2980b03f6d3e2739
egultekins <new-email@example.com>
2025-02-07 16:50:13 +0300

    Project development has started

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react/jsx-runtime";
+import Header from "./components/Header";
+
+
+
+const App: React.FC = () => {
+
+
+  return (
+    <Fragment>
+      
+      <Header/>
+
+    </Fragment> 
+    
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { Fragment } from "react/jsx-runtime";
+import "../App.css"; 
+function Header(){
+
+    return(
+        
+
+
+        <Fragment>
+        
+           
+           
+                <div className="Navs max-h-20 px-10.5 py-6 flex content-center items-center justify-between ">
+                    <span className="logo font-semibold">Portfolio</span>
+                    <ul className="flex content-center items-center justify-end gap-6" >
+                        <li>About</li>
+                        <li>Work</li>
+                        <li>Contact</li>
+                    </ul>
+
+
+
+                </div>
+              
+            
+            
+            
+        </Fragment>
+    );
+
+}
+
+export default Header;
